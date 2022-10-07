06bab69a26aa561e1e947c4a439b965d04edc42f
Alex Agape <83664587+y3rsso@users.noreply.github.com>
2022-10-07 15:06:16 +0300

    Update quick_sort.py

--- Laborator1/quick_sort.py
+++ Laborator1/quick_sort.py
@@ -1,13 +1,24 @@
-number_list = []
-n = int(input("Insert # of elements: "))
-print("\n")
-for i in range(0,n):
-    print("Insert element for: ", i, " ")
-    item = int(input())
-    number_list.append(item)
-print("The list is", number_list)
-for i in range(n):
-    for j in range(0, n-i-1):
-        if number_list[j] > number_list[j+1]:
-            number_list[j], number_list[j+1] = number_list[j+1], number_list[j]
-print("Sorted list is:", number_list)
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+ 
+        quickSort(array, pi + 1, high)
+ 
+data = [1, 7, 4, 1, 10, 9, -2]
+print("Unsorted Array")
+print(data)
+size = len(data)
+quickSort(data, 0, size - 1)
+print('Sorted Array in Ascending Order:')
+print(data)
